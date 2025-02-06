023992e4ed404032c33077b88368fe0190b5a38f
balguri-sony <sony.b@pravaltech.com>
2025-02-06 18:25:45 +0530

    changed code

--- src/test/java/GitExamaple/GitExample/GitEx.java
+++ src/test/java/GitExamaple/GitExample/GitEx.java
@@ -4,5 +4,6 @@ public class GitEx {
 	public static void main(String[] args) {
 		System.out.println("Explaining about GitHub Commands");
 		System.out.println("git init");
+		System.out.println("changed code");
 	}
 }
